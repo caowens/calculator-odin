f2e1a69e79bbc38661a8dbc8aa0c3c432a72adde
caowens <caowens555@yahoo.com>
2024-03-13 19:48:09 -0500

    Fill out calculator logic

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let numberA = null;
+let operator = null;
+let numberB = null;
+
 const add = function(a, b) {
 	return a + b;
 };
@@ -14,37 +18,102 @@ const divide = function(a, b) {
   return a / b;
 };
 
+function Display() {
+    this.val = '';
+    this.display = document.querySelector('#display');
+    this.inputStack = [];
+    this.updateDisplay = function(val) {
+        if (this.val.length < 11) {
+            this.val += val;
+            this.display.innerText = this.val;
+        }
+    }
+    this.clearDisplay = function() {
+        this.val = '';
+        this.display.innerText = '0';
+    }
+}
 
-let numberA;
-let operator;
-let numberB;
+let display = new Display();
 
 function operate(num1, op, num2) {
     switch (op) {
         case '+':
-            return add(num1, num2);
+            display.clearDisplay();
+            numberA = add(+num1, +num2);
+            display.updateDisplay(numberA.toString());
+            break;
         case '-':
-            return subtract(num1, num2);
+            display.clearDisplay();
+            numberA = subtract(+num1, +num2);
+            display.updateDisplay(numberA);
+            break;
         case 'x':
-            return multiply(num1, num2);
+            display.clearDisplay();
+            numberA = multiply(+num1, +num2);
+            display.updateDisplay(numberA);
+            break;
         case '/':
-            return divide(num1, num2);
-        default:
-          return "Error: invalid input for operate()";
-      }
-}
-
-let displayValue = 0;
-
-function updateDisplay(val) {
-    const display = document.querySelector('#display');
-    display.innerText = val;
-    displayValue = val;
+            display.clearDisplay();
+            numberA = divide(+num1, +num2);
+            display.updateDisplay(numberA);
+            break;
+    }
 }
 
 const numbers = Array.from(document.querySelectorAll('.operand'));
 numbers.map((btn) => {
     btn.addEventListener('click', () => {
-        updateDisplay(btn.innerText)
-    })
+        display.updateDisplay(btn.innerText);
+    });
+});
+
+const operators = Array.from(document.querySelectorAll('.operator'));
+operators.map((btn) => {
+    btn.addEventListener('click', () => {
+        operator = btn.innerText;
+        if (numberA === null) {
+            numberA = display.val;
+            operator = btn.innerText;
+            display.inputStack.push(operator);
+            display.val = '';
+        }
+        else if (numberB === null) {
+            numberB = display.val;
+            operate(numberA, operator, numberB);
+            numberB = null;
+            display.val = '';
+        }
+        else {
+            operate(numberA, operator, numberB);
+            numberB = null;
+        }
+    });
+});
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', () => {
+    if (numberA && !numberB && operator) {
+        numberB = display.val;
+        console.log(`${numberA} ${operator} ${numberB}`);
+        operate(numberA, operator, numberB);
+        numberB = null;
+        operator = null;
+        display.inputStack = [];
+    }
+});
+
+const clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+    display.clearDisplay();
+    numberA = null;
+    operator = null;
+    numberB = null;
+});
+
+const decimalBtn = document.querySelector('#decimal');
+decimalBtn.addEventListener('click', () => {
+    if (display.val[-1] !== '.') {
+        display.updateDisplay('.');
+    }
 });
